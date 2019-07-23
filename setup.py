ae1356fceafb586fe4627ba9ad4f4426529965b4
Tree <tree@ejyi.com>
2019-07-23 15:36:46 +0800

    调整目录结构

--- setup.py
+++ setup.py
@@ -14,14 +14,33 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/treeyh/soc-excel-convert",
-    keywords="soc excel convert markdown json ",
-    packages=find_packages(),
+    keywords=["soc", "excel", "convert", "markdown"],
+    # packages=['src'],
+    package_dir={"": "src"},
+    packages=find_packages(
+        where="src",
+        exclude=["docs"],
+    ),
+    license="Apache License Version 2.0",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Apache License Version 2.0",
         "Operating System :: OS Independent",
     ],
 
+    package_data={
+        "pip._vendor.certifi": ["*.pem"],
+        "pip._vendor.requests": ["*.pem"],
+        "pip._vendor.distlib._backport": ["sysconfig.cfg"],
+        "pip._vendor.distlib": ["t32.exe", "t64.exe", "w32.exe", "w64.exe"],
+    },
+    entry_points={
+        "console_scripts": [
+            "soc-excel-convert=soc-excel-convert._internal:main",
+        ],
+    },
+
+    install_requires=['et-xmlfile>=1.0.1', 'jdcal>=1.4.1', 'openpyxl>=2.6.2'],
     zip_safe=False,
-    python_requires='>=3.0,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='>=3',
 )
